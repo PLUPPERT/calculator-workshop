473a913b8004ebd8f94260e9570070316a5b4df9
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-15 14:27:29 +0100

    - First commit after power outage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        boolean runApp = true;
+        int num1;
+        int num2;
+        int operation;
 
 
+
+        System.out.println("Enter first number");
+        num1 = scanner.nextInt();
+        System.out.println("Enter second number");
+        num2 = scanner.nextInt();
+        System.out.println("Choose operation");
+        System.out.println("1 : Addition\n2 : Subtraction\n3 : Multiply\n4 : Divide\n0 : Exit");
     }
 }
