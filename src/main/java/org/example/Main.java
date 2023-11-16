d7492cd6999bb76a5dd8999114f6b2052f8460b8
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-16 15:33:08 +0100

    - Clean up by:  - Moving code out of main in external methods  - Remove obsolete code by simplifying functionality.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,91 +5,83 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-        boolean runApp;
-        boolean availableOption;
         int num1;
         int num2;
         int operation;
-        String intParsedToString;
 
-            do {
-                runApp = true;
-                printOptions();
-                operation = scanner.nextInt();
-                availableOption = (operation >= 0) && (operation <=4);
+        while (true) {
+            System.out.println("Welcome to the CALCULATOR ! ! !");
+            printOptions();
+            operation = userInput(1);
+            if (operation == 0) {
+                System.out.println("Turning off Calculator...");
+                TimeUnit.SECONDS.sleep(2);
+                break;
+            }
+            System.out.print("Enter first number: ");
+            num1 = userInput(2);
+            System.out.print("Enter second number: ");
+            num2 = userInput(3);
 
-                intParsedToString = String.valueOf(operation);
-                while (!isInteger(intParsedToString) || !availableOption) {
-                    System.out.println("Invalid input. Please try again.");
-                    printOptions();
-                    operation = scanner.nextInt();
-                    availableOption = (operation >= 0) && (operation <=4);
-                }
-
-                if (operation == 0) {
-                    System.out.println("Turning off Calculator...");
-                    TimeUnit.SECONDS.sleep(3);
-                    runApp = false;
-                }
-
-                if (runApp) {
-
-                    do {
-                        System.out.println("Enter first number: ");
-                        num1 = scanner.nextInt();
-                        intParsedToString = String.valueOf(num1);
-                    } while (!isInteger(intParsedToString));
-                    do {
-                        System.out.println("Enter second number: ");
-                        num2 = scanner.nextInt();
-                        intParsedToString = String.valueOf(num2);
-                    } while (!isInteger(intParsedToString));
-
-                    switch (operation) {
-                        case 1:
-                            calculate(num1, num2, "+");
-                            break;
-                        case 2:
-                            calculate(num1, num2, "-");
-                            break;
-                        case 3:
-                            calculate(num1, num2, "*");
-                            break;
-                        case 4:
-                            calculate(num1, num2, "/");
-                            break;
-                        default:
-                            System.out.println("Something went wrong. Please try again");
-                    }
-                }
-
-            } while (runApp);
-
+            calculate(num1, num2, operation);
+        }
     }
-    
+
     public static void printOptions() {
         System.out.println("Options:");
         System.out.println("1 : Addition\n2 : Subtraction\n3 : Multiply\n4 : Divide\n0 : Exit");
-        System.out.print("Choose Option hand hit ENTER: ");
+        System.out.print("Choose Option and hit ENTER: ");
     }
-    public static boolean isInteger(String s) {
-        try {
-            Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            System.out.println(s + ": is not a valid input. Try again, please.");
+
+    public static int userInput(int inputEntryNr) {
+        Scanner input = new Scanner(System.in);
+        boolean validInput = false;
+        String instruction;
+        String inputValue = null;
+        int validatedValue;
+
+        while (!validInput) {
+            inputValue = input.nextLine();
+            try {
+                validatedValue = Integer.parseInt(inputValue);
+                if (inputEntryNr == 1) {
+                    validInput = validOptionInput(validatedValue);
+                } else {
+                    validInput = true;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("\"" + inputValue + "\"" + " : is not a valid input. Try again, please.");
+                if (inputEntryNr == 1) {
+                    printOptions();
+                } else {
+                    instruction = (inputEntryNr == 2) ? "Enter first number: " : "Enter second number: ";
+                    System.out.print(instruction);
+                }
+            }
+        }
+        return Integer.parseInt(inputValue);
+    }
+
+    public static boolean validOptionInput(int option) {
+        if ((option < 0) || (option > 4)) {
+            System.out.println(option + ": is not a valid input. Try again, please.");
+            printOptions();
             return false;
         }
         return true;
     }
 
-    public static void calculate(int num1, int num2, String operation) {
+    public static void calculate(int num1, int num2, int operation) throws InterruptedException {
+        String resultString = "Result -> ";
         switch (operation) {
-            case "+" -> System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-            case "-" -> System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-            case "*" -> System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-            case "/" -> System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+            case 1 -> System.out.println(resultString + num1 + " + " + num2 + " = " + (num1 + num2));
+            case 2 -> System.out.println(resultString + num1 + " - " + num2 + " = " + (num1 - num2));
+            case 3 -> System.out.println(resultString + num1 + " * " + num2 + " = " + (num1 * num2));
+            case 4 -> System.out.println(resultString + num1 + " / " + num2 + " = " + (num1 / num2));
             default -> System.out.println("Something went wrong");
         }
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Starting new Calculator session...");
+        TimeUnit.SECONDS.sleep(2);
     }
 }
